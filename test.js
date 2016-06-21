9a99f76d909048527f781776b6245d1d2fbed5ff
NoizBuster <NoizBuster@NoizBuster.com>
2016-06-21 09:36:05 +0900

    added test code and files

---
+++ test.js
@@ -0,0 +1,11 @@
+var config = require('./default');
+
+var TEST_DEFAULT_FILE_PATH = './tests/default_sample.json';
+var TEST_CONFIG_FILE_PATH = './tests/user_config.json';
+
+var configManager = new config(TEST_DEFAULT_FILE_PATH, TEST_CONFIG_FILE_PATH);
+configManager.initSync();
+console.log('CONFIG: ', configManager.CONFIG_PATH);
+console.log('DEFAULT: ', configManager.DEFAULT_PATH);
+var configs = configManager.getConfigsSync();
+console.log(configs);
