1ee73ed21bda0cef9ab7bdae62c10deb7dd50949
NoizBuster <NoizBuster@NoizBuster.com>
2016-07-18 14:59:06 +0900

    updated test code

--- test.js
+++ test.js
@@ -1,15 +1,50 @@
 var config = require('./default');
-
+var fse = require('fs-extra');
 var TEST_DEFAULT_FILE_PATH = './tests/default_sample.json';
 var TEST_CONFIG_FILE_PATH = './tests/user_config.json';
 
+var TEST_DEFAULT_YAML_FILE_PATH = './tests/default_sample.yaml';
+var TEST_CONFIG_YAML_FILE_PATH = './tests/user_config.yaml';
+
+fse.removeSync(TEST_CONFIG_FILE_PATH);
+fse.removeSync(TEST_CONFIG_YAML_FILE_PATH);
+
+console.info('\nJSON test ===========================================================');
 var configManager = new config(TEST_DEFAULT_FILE_PATH, TEST_CONFIG_FILE_PATH);
-configManager.initSync();
 console.log('CONFIG: ', configManager.CONFIG_PATH);
 console.log('DEFAULT: ', configManager.DEFAULT_PATH);
-var configs = configManager.getConfigsSync();
-console.log(configs);
 
-configManager.setConfigSync("testString", 'new value !!!');
-var configse = configManager.getConfigsSync();
-console.log(configse);
+console.info('\nLoad default configuration file======================================');
+configManager.initSync();
+
+console.info('\nLoad again configuration file========================================');
+configManager.initSync();
+
+console.info('\nRead Configs=========================================================');
+console.log(configManager.readConfigsSync());
+console.info('\nset Configs==========================================================');
+configManager.setConfigSync('testString', 'new value !!!');
+console.log(configManager.readConfigsSync());
+
+console.info('\nJDON Test Ended======================================================\n\n\n');
+
+
+console.info('\nYAML test ===========================================================');
+var yamlConfigManager = new config(TEST_DEFAULT_YAML_FILE_PATH, TEST_CONFIG_YAML_FILE_PATH);
+console.log('CONFIG: ', yamlConfigManager.CONFIG_PATH);
+console.log('DEFAULT: ', yamlConfigManager.DEFAULT_PATH);
+
+console.info('\nLoad default configuration file======================================');
+yamlConfigManager.initSync();
+
+console.info('\nLoad again configuration file========================================');
+yamlConfigManager.initSync();
+
+console.info('\nRead Configs=========================================================');
+console.log(yamlConfigManager.readConfigsSync());
+
+console.info('\nset Configs==========================================================');
+yamlConfigManager.setConfigSync('testString', 'new value !!!');
+console.log(yamlConfigManager.readConfigsSync());
+
+console.info('\nYAML Test Ended======================================================');
